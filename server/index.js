2d8f765f572b05932a2e80ca643704eb12abe9f7
BellaCOCONUTS <jjgransbury@hotmail.com>
2022-10-03 20:09:28 +0100

    updated app login

--- server/index.js
+++ server/index.js
@@ -15,7 +15,8 @@ const port = process.env.PORT || 3000
 
 const client_id = process.env.CLIENT_ID
 const client_secret = process.env.CLIENT_SECRET
-const redirect_uri = 'http://localhost:3000/callback'
+const redirect_uri =
+  `${process.env.REDIRECT_URL}/callback` || 'http://localhost:3000/callback'
 
 app.get('/login', (_, res) => {
   console.log('handling log in')
--- src/Login.tsx
+++ src/Login.tsx
@@ -2,7 +2,10 @@ import { useEffect } from 'react'
 
 export function Login() {
   useEffect(() => {
-    window.location.href = `http://localhost:3000/login`
+    window.location.href =
+      window.location.hostname === 'localhost'
+        ? 'http://localhost:3000/login'
+        : 'https://web-dedupe-sptfy-app.azurewebsites.net/login'
   }, [])
 
   return <></>
